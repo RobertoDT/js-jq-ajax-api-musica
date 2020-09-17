1baa63d5b5a7dc2b49c0e206184bbea060328ae0
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-17 19:02:46 +0200

    Bonus Esercizio Musica

--- js/main.js
+++ js/main.js
@@ -7,14 +7,16 @@ $(document).ready(function() {
       "success": function (data, stato) {
         var response = data.response;
 
+        //ciclo ogni disco presente nell'array
         for(var i = 0; i < response.length; i++){
           var dischi = response[i];
 
+          //preparo e compilo il mio template
           var source = $("#music-template").html();
           var template = Handlebars.compile(source);
 
           var html = template(dischi);
-
+          //scrivo nel cds-container il contenuto del mio template
           $(".cds-container").append(html);
         }
 
@@ -25,5 +27,35 @@ $(document).ready(function() {
     }
   );
 
+  $("#musical-genre").change(function(){
+
+    // alert('Selected value: ' + $(this).val());
+
+    $.ajax(
+      {
+        "url": "https://flynn.boolean.careers/exercises/api/array/music",
+        "method": "GET",
+        "success": function (data, stato) {
+
+          var genereMusicale = $("#musical-genre").val();
+
+          var response = data.response;
+
+          for(var i = 0; i < response.length; i++){
+            var dischi = response[i].genre;
+            var myClass = "." + dischi;
+            $(myClass).hide();
+          }
+
+          $("." + genereMusicale).show();
+
+        },
+        "error": function (richiesta, stato, errori) {
+          alert("E' avvenuto un errore. " + errori);
+        }
+      }
+    );
+
+});
 
 });
